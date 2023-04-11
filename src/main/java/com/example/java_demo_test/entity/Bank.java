b70ca35004ea349982a2e9315eea390069aecc92
Hitsuki <gg0880933@gmail.com>
2023-04-11 11:38:23 +0800

    add bank enity

---
+++ src/main/java/com/example/java_demo_test/entity/Bank.java
@@ -0,0 +1,27 @@
+package com.example.java_demo_test.entity;
+
+public class Bank {
+	
+	private int balance;
+	
+	private int amount;
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
